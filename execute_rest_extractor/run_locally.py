f5c3fe64a1a34a7f3f4f74bed550d33040efde15
Christian Flasshoff <71002867+chrfla-cognite@users.noreply.github.com>
2022-12-02 19:38:00 +0100

    add local run

---
+++ execute_rest_extractor/run_locally.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+from ice_cream_factory_datapoints_extractor import extractor
+
+
+# Import Environment Variables
+load_dotenv(".env")
+
+
+def handle(data):
+    print("running rest extractor locally")
+    os.environ["FRONTFILL_LOOKBACK_MIN"] = data.get("frontfill_lookback_min")
+    extractor.main(config_file_path="execute_rest_extractor/extractor_config.yaml")
+    print("running rest extractor locally is done")
+
+
+if __name__ == "__main__":
+    data = {"frontfill_lookback_min": "3600"}
+    handle(data)
