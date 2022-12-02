640aa92709b61c02b93d23553d8b253c9b2f35ff
Christian Flasshoff <71002867+chrfla-cognite@users.noreply.github.com>
2022-12-02 14:21:19 +0100

    disable unit tests

--- tests/example_function1/test_example_function1.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from example_function1.handler import handle
-
-
-@pytest.mark.unit
-def test_handler(mocked_data, cognite_client_mock):
-    result = handle(mocked_data, cognite_client_mock)
-    assert result == mocked_data
