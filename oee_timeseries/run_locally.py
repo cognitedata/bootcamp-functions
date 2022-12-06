5c4afc2dbf261e557bdc5f1de613cf9cbd048efc
Christian Flasshoff <71002867+chrfla-cognite@users.noreply.github.com>
2022-12-06 17:06:10 +0100

    fix bugs

--- oee_timeseries/run_locally.py
+++ oee_timeseries/run_locally.py
@@ -7,5 +7,7 @@ from oee_timeseries.handler import handle
 # Import Environment Variables
 load_dotenv()
 client = get_client()
-data = {"sites": ["Oslo", "Hannover"], "lookback_minutes": 60, "data_set_external_id": "uc:001:oee:ds"}
+data = {"sites": ["Oslo", "Hannover", "Nuremberg"], "lookback_minutes": 60, "data_set_external_id": "uc:001:oee:ds"}
+
+# Delete Timeseries
 handle(client, data)
