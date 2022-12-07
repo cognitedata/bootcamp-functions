704d38c1ba4011986c3e70ef3b992cc699190bf1
Pavel Zubarev <pavel.zubarev@cognite.com>
2022-12-07 17:06:27 +0100

    Add fixes

--- oee_timeseries/handler.py
+++ oee_timeseries/handler.py
@@ -9,10 +9,18 @@ import arrow
 import numpy as np
 from arrow import Arrow
 from cognite.client import CogniteClient
+<<<<<<< Updated upstream
 from tools import discover_datapoints
 from tools import get_timeseries_for_site
 from tools import insert_datapoints
 
+=======
+
+from oee_timeseries.tools import discover_datapoints
+from oee_timeseries.tools import get_timeseries_for_site
+from tools import insert_datapoints
+
+>>>>>>> Stashed changes
 CYCLE_TIME = 3
 
 
@@ -38,15 +46,16 @@ def handle(client: CogniteClient, data: Dict[str, Any]) -> None:
     print(f"Input data of function: {data}")
 
     # Input data
-    lookback_minutes = data.get("lookback_minutes", 60)
+    lookback_minutes = data.get("lookback_minutes", 1440)
+    window_size = data.get("window_size_minutes", 60)
     data_set_external_id = data.get("data_set_external_id", "uc:001:oee:ds")
     sites = data.get("sites")
     # now specifies the time upto which the OEE numbers will be calculated
     # We want to balance the data freshness here
     the_latest = get_state(client, db_name="src:002:opcua:db:state", table_name="timeseries_datapoints_states")
-    now = arrow.get(the_latest).shift(minutes=-10)
+    now = arrow.get(the_latest).floor("minutes").shift(minutes=-10)  # -10 minutes as a safety margin
 
-    window = (now.shift(minutes=-2 * lookback_minutes), now)
+    window = (now.shift(minutes=- (lookback_minutes + window_size)), now)
 
     data_set = client.data_sets.retrieve(external_id=data_set_external_id)
     for site in sites:
@@ -68,30 +77,30 @@ def handle(client: CogniteClient, data: Dict[str, Any]) -> None:
             planned_uptime_points = np.array(discovered_points.get(f"{item}:planned_status"))[:, 1]
 
             if (
-                len(total_items) != len(good_items)
-                or len(total_items) != len(uptime_points)
-                or len(total_items) != len(planned_uptime_points)
+                    len(total_items) != len(good_items)
+                    or len(total_items) != len(uptime_points)
+                    or len(total_items) != len(planned_uptime_points)
             ):
                 raise RuntimeError(f"CDF returned different amount of aggregations for {window}")
 
-            if len(total_items) != 2 * lookback_minutes:
+            if len(total_items) != lookback_minutes + window_size:
                 raise RuntimeError(
                     f"CDF returned inaccurate amount of aggregations for {window}. "
-                    f"Got {len(total_items)}, expected {2 * lookback_minutes}"
+                    f"Got {len(total_items)}, expected {lookback_minutes + window_size}"
                 )
 
             bad_items = np.subtract(total_items, good_items)
             quality = np.divide(good_items, total_items, out=np.zeros_like(good_items), where=total_items != 0)
-            uptime = np.convolve(uptime_points, np.ones(lookback_minutes, dtype=int), "valid")[1:]
-            produced = np.convolve(total_items, np.ones(lookback_minutes, dtype=int), "valid")[1:]
-            ideal_rate = np.full(lookback_minutes, 60.0 / 3.0)  # we know that ideal production should be 3 per sec
-            planned_uptime = np.convolve(planned_uptime_points, np.ones(lookback_minutes, dtype=int), "valid")[1:]
+            uptime = np.convolve(uptime_points, np.ones(window_size, dtype=int), "valid")[1:]
+            produced = np.convolve(total_items, np.ones(window_size, dtype=int), "valid")[1:]
+            ideal_rate = np.full(lookback_minutes, window_size * (60.0 / 3.0))  # we know that ideal production should be 3 per sec
+            planned_uptime = np.convolve(planned_uptime_points, np.ones(window_size, dtype=int), "valid")[1:]
 
             off_spec_dps.extend(
                 [
                     {
                         "externalId": f"{item}:off_spec",
-                        "datapoints": get_payload(lambda x, off=bad_items: off[x], lookback_minutes, now),
+                        "datapoints": get_payload(lambda x, off=bad_items[window_size:]: off[x], lookback_minutes, now),
                     }
                 ]
             )
@@ -100,7 +109,7 @@ def handle(client: CogniteClient, data: Dict[str, Any]) -> None:
                 [
                     {
                         "externalId": f"{item}:quality",
-                        "datapoints": get_payload(lambda x, q=quality: q[x], lookback_minutes, now),
+                        "datapoints": get_payload(lambda x, q=quality[window_size:]: q[x], lookback_minutes, now),
                     }
                 ]
             )
@@ -138,9 +147,8 @@ def handle(client: CogniteClient, data: Dict[str, Any]) -> None:
                     {
                         "externalId": f"{item}:oee",
                         "datapoints": get_payload(
-                            lambda x, q=quality, p=produced, r=ideal_rate, pl=planned_uptime: (q[x] * p[x] * r[x])
-                            / pl[x]
-                            if pl[x] != 0
+                            lambda x, q=quality[window_size:], p=produced, r=ideal_rate, pl=planned_uptime, up=uptime: q[x] * (p[x] / up[x] / r[x]) * (up[x] / pl[x])
+                            if pl[x] != 0 and up[x] != 0 and r[x] != 0
                             else 0.0,
                             lookback_minutes,
                             now,
