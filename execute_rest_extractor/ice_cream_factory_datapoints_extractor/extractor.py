df87f9847ca53ec05fbcfac3a8487ebfce2e9c9c
Christian Flasshoff <71002867+chrfla-cognite@users.noreply.github.com>
2022-12-07 14:37:36 +0100

    fix sites

--- execute_rest_extractor/ice_cream_factory_datapoints_extractor/extractor.py
+++ execute_rest_extractor/ice_cream_factory_datapoints_extractor/extractor.py
@@ -1,4 +1,5 @@
 import argparse
+import ast
 import logging
 import os
 import random
@@ -69,11 +70,12 @@ def run_extractor(
         stop_event: Cancellation token, will be set when an interrupt signal is sent to the extractor process
     """
 
-    config.frontfill.enabled = os.getenv("FRONTFILL_ENABLED", config.frontfill.enabled).lower() == "true"
+    config.frontfill.enabled = os.getenv("FRONTFILL_ENABLED", config.frontfill.enabled)
     config.frontfill.lookback_min = int(os.getenv("FRONTFILL_LOOKBACK_MIN", config.frontfill.lookback_min))
-    config.backfill.enabled = os.getenv("BACKFILL_ENABLED", config.backfill.enabled).lower() == "true"
+    config.backfill.enabled = os.getenv("BACKFILL_ENABLED", config.backfill.enabled)
     config.backfill.history_days = int(os.getenv("BACKFILL_HISTORY_DAYS", config.backfill.history_days))
-    config.api.sites = os.getenv("SITES", config.api.sites)
+    if os.getenv("SITES"):
+        config.api.sites = ast.literal_eval(os.getenv("SITES"))
 
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting Ice Cream Factory datapoints extractor")
